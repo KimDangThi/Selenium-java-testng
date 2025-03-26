a688466ca42785d7797e08d56d84cf51303d3028
Kim Dang <kimdt.neu@gmail.com>
2025-03-26 22:58:42 +0700

    Initial commit

--- selenium/webdriver/Topic_01_Check_Environment.java
+++ selenium/webdriver/Topic_01_Check_Environment.java
@@ -19,7 +19,7 @@ public class Topic_01_Check_Environment {
     public void TC_02_Run_On_Chrome() {
         driver = new ChromeDriver();
         driver.get("https://www.facebook.com/");
-        driver.quit();
+        driver.quit();//
     }
     @Test
     public void TC_03_Run_On_Edge() {
